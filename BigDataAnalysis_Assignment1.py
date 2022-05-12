74f942ed5b1742ebdbe74d4405e4faf426e043e6
David C. Li <sunnydavidli@gmail.com>
2022-05-12 11:36:09 +0800

    create a simple linechart for dataframe.

--- BigDataAnalysis_Assignment1.py
+++ BigDataAnalysis_Assignment1.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import datetime
+import matplotlib.pyplot as plt
 
-time = datetime.date()
 
-print(time)
+# time = datetime.date()
+
+# print(time)
 
 students = pd.read_csv("./datasets/students.csv")
 
@@ -14,4 +16,8 @@ print()
 # Return the top 5 total scores with the most occurrences
 print(students['G3'].value_counts().head(5))
 print()
+
+# plot something
+students.plot()
+plt.show()
 
